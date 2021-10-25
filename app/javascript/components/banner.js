af60f85f8b2928e40b2c943b7ab936a603a8c00e
Wilson Filho <wilsonsenhor@gmail.com>
2021-10-25 12:32:00 -0300

    add banner

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
     strings: ["Save any kind", "of movie"],
-    typeSpeed: 100,
+    typeSpeed: 80,
     loop: true
   });
 }
